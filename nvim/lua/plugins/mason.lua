44f4b06aa04373aacae395aafa7bc1a3d2931214
Darshan Gad <darshangad@gmail.com>
2025-09-07 20:05:36 +0200

    fixed typo for jsonlsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,30 +1,8 @@
 return {
 	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"bashls",
-				"tailwindcss",
-				"cmake",
-				"clangd", -- C++,C
-				"gopls", -- Go
-				"templ", -- Templ
-				"pyright", -- Python
-				"lua_ls", -- Lua
-				"yamlls", -- YAML
-				"lemminx", --XML
-				"sqlls", --SQL
-				"lsonls", --JSON
-				"html",
-				"dockerls",
-				"cssls",
-				"ansiblels",
-
-			},
-		},
-		dependencies = {
-			{ "williamboman/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
 	}
 }
---
+++ nvim/lua/plugins/mason_lsp.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"bashls",
+				"tailwindcss",
+				"cmake",
+				"clangd", -- C++,C
+				"gopls", -- Go
+				"templ", -- Templ
+				"pyright", -- Python
+				"lua_ls", -- Lua
+				"yamlls", -- YAML
+				"lemminx", --XML
+				"sqlls", --SQL
+				"jsonls", --JSON
+				"html",
+				"dockerls",
+				"cssls",
+				"ansiblels",
+
+			},
+		},
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	}
+}
