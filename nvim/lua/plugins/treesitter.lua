b1c00c691fa854527f525ad09bb9d0a3c6de67db
Darshan Gad <dgad@markem-imaje.com>
2026-06-12 11:20:29 +0200

    update clang to better handle const

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "templ",
+	callback = function()
+		vim.bo.commentstring = "<!-- %s -->"
+	end,
+})
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "master",
@@ -6,8 +13,23 @@ return {
 	config = function()
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
-				"c", "cpp", "lua", "vim", "vimdoc", "query", "cmake", "bash", "python", "go",
-				"markdown", "markdown_inline", "regex", "templ", "html", "dockerfile", "yaml"
+				"c",
+				"cpp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"cmake",
+				"bash",
+				"python",
+				"go",
+				"markdown",
+				"markdown_inline",
+				"regex",
+				"templ",
+				"html",
+				"dockerfile",
+				"yaml",
 			},
 			auto_install = true,
 			highlight = { enable = true },
